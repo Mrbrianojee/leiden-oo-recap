120cd15aa54ec6e49bc45bf3d2ad1a48b0f0704f
Brianojee <brian@codeinstitute.net>
2018-07-08 13:56:53 +0000

    fixed missing self prepends in contsructor and added description method

--- bird.py
+++ bird.py
@@ -3,8 +3,9 @@ class Bird:
     _the_call =  None
     
     def __init__(self, kind, call):
-        _the_kind =  kind
-        _the_call =  call
-    
-   
+        self._the_kind =  kind
+        self._the_call =  call
+
+    def description(self):
+        print('A {0} goes {1}').format(self._the_kind, self._the_call)
         
---
+++ bird_builder.py
@@ -0,0 +1,6 @@
+from bird import Bird
+ 
+my_bird = Bird("Owl", "Twit-Twoo!")
+
+print(my_bird._the_kind)
+print(my_bird._the_call)
