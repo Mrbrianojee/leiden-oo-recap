d8cfb6b8c63dd995115019bf8501fb8546e9ef8c
Brianojee <brian@codeinstitute.net>
2018-07-08 13:59:28 +0000

    refactored description method to return string rather than just print string

--- bird.py
+++ bird.py
@@ -7,5 +7,5 @@ class Bird:
         self._the_call =  call
 
     def description(self):
-        print('A {0} goes {1}').format(self._the_kind, self._the_call)
+        return 'A {0} goes {1}'.format(self._the_kind, self._the_call)
         
--- bird_builder.py
+++ bird_builder.py
@@ -2,5 +2,7 @@ from bird import Bird
  
 my_bird = Bird("Owl", "Twit-Twoo!")
 
-print(my_bird._the_kind)
-print(my_bird._the_call)
+# print(my_bird._the_kind)
+# print(my_bird._the_call)
+
+print(my_bird.description())
